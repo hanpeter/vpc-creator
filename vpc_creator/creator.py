b3fe31ce1bb869194d2694cc7a4861fac135f83c
Peter Han <git@peterhan.me>
2018-10-01 18:33:32 -0700

    Skeleton code

---
+++ vpc_creator/creator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+
+class Creator(object):
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        print('Hello World')
