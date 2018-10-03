199350f656187785868986415371c7525bedc0ee
Peter Han <git@peterhan.me>
2018-10-02 22:56:48 -0700

    Adding dummy tests

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class TestCli(unittest.TestCase):
+
+    def test_main(self):
+        pass
---
+++ tests/test_creator.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class TestCreator(unittest.TestCase):
+
+    def test_init(self):
+        pass
