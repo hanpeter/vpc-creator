faf9456b800ce528b42a63276dbd527bee7dda03
Peter Han <git@peterhan.me>
2018-10-03 20:55:47 -0700

    Add 2 CloudWatch alarms to NAT gateways

--- vpc_creator/cli.py
+++ vpc_creator/cli.py
@@ -17,8 +17,9 @@ CONTEXT_SETTINGS = {
 @click.option('--cidr', '-c', type=str, default='10.0.0.0/16', show_default=True)
 @click.option('--creator', type=str, default=getpass.getuser(), show_default=True)
 @click.option('--subnet', '-s', required=True, multiple=True, type=str, nargs=2)
-def main(name, cidr, creator, subnet):
+@click.option('--sns', default=None, show_default=True, type=str)
+def main(name, cidr, creator, subnet, sns):
     subnets = {s[0]: s[1] for s in subnet}
 
     crt = Creator()
-    crt.run(name=name, cidr=cidr, subnets=subnets, creator=creator)
+    crt.run(name=name, cidr=cidr, subnets=subnets, creator=creator, sns=sns)
